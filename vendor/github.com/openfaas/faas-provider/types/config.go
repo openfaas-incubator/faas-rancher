5ad8e0438963f820c784dceff3b4ce7cf0be3614
Benjamin Seibert <benjamin.seibert@roomthirteen.de>
2019-01-25 17:35:02 +0100

    Updated openfaas vendors
    Use openfaas 0.8.1 vendors.
    
    Signed-off-by: Benjamin Seibert <benjamin.seibert@roomthirteen.de>

--- vendor/github.com/openfaas/faas-provider/types/config.go
+++ vendor/github.com/openfaas/faas-provider/types/config.go
@@ -9,15 +9,26 @@ import (
 type FaaSHandlers struct {
 	FunctionReader http.HandlerFunc
 	DeployHandler  http.HandlerFunc
+	// FunctionProxy provides the function invocation proxy logic.  Use proxy.NewHandlerFunc to
+	// use the standard OpenFaaS proxy implementation or provide completely custom proxy logic.
+	FunctionProxy  http.HandlerFunc
 	DeleteHandler  http.HandlerFunc
 	ReplicaReader  http.HandlerFunc
-	FunctionProxy  http.HandlerFunc
 	ReplicaUpdater http.HandlerFunc
+	SecretHandler  http.HandlerFunc
+
+	// Optional: Update an existing function
+	UpdateHandler http.HandlerFunc
+	Health        http.HandlerFunc
+	InfoHandler   http.HandlerFunc
 }
 
 // FaaSConfig set config for HTTP handlers
 type FaaSConfig struct {
-	TCPPort      *int
-	ReadTimeout  time.Duration
-	WriteTimeout time.Duration
+	TCPPort         *int
+	ReadTimeout     time.Duration
+	WriteTimeout    time.Duration
+	EnableHealth    bool
+	EnableBasicAuth bool
+	SecretMountPath string
 }
